3f986d0fc8aa6e6310e920f30e94706e35b9cf0e
rsheridan613 <rsheridan613@gmail.com>
2022-11-17 20:21:00 -0700

    added prompt and questions

--- index.js
+++ index.js
@@ -3,7 +3,55 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is the title of the project?",
+  "Type a short description for your project.",
+  "What are the installation instructions?",
+  "How do you use this app?",
+  "Who are the contributors?",
+  "test thing",
+  "Which liscense would you like?",
+  "What is your GitHub username?",
+  "What is your email?",
+];
+inquirer
+  .prompt([
+    {
+      name: "title",
+      type: "input",
+      message: questions[0],
+    },
+    {
+      name: "description",
+      type: "input",
+      message: questions[1],
+    },
+    {
+      name: "install",
+      type: "input",
+      message: questions[2],
+    },
+    { name: "usage", type: "input", message: questions[3] },
+    { name: "contribution", type: "input", message: questions[4] },
+    {
+      name: "test",
+      type: "input",
+      message: questions[5],
+    },
+    {
+      name: "liscense",
+      type: "list",
+      message: questions[6],
+      choices: ["one", "two", "three"],
+    },
+    {
+      name: "gitUsername",
+      type: "input",
+      message: questions[7],
+    },
+    { name: "email", type: "input", message: questions[8] },
+  ])
+  .then((response) => console.log(response));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
